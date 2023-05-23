6d2ec7f745aba3ed46149e7fa5362ea6d52a6107
zeinabansarian <zeinabansarian6280@gmail.com>
2023-05-23 17:01:03 +0330

    start & create background & coding menu

---
+++ src/Component/Sidebar/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Sidebar() {
+  return (
+    <div className='sidebar'>
+        <div className='menuSidebar'>
+            <div>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
---
+++ src/Component/background/Background.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './background.css'
+export default function Background() {
+  return (
+    <div>
+       <div class='light x1'></div>
+        <div class='light x2'></div>
+        <div class='light x3'></div>
+        <div class='light x4'></div>
+        <div class='light x5'></div>
+        <div class='light x6'></div>
+        <div class='light x7'></div>
+        <div class='light x8'></div>
+        <div class='light x9'></div>
+        
+        <div class='light x1'></div>
+        <div class='light x2'></div>
+    </div>
+  )
+}
