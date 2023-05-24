d63955daac9ac94fea83b63776cfff36288a31a6
zeinabansarian <zeinabansarian6280@gmail.com>
2023-05-24 17:44:41 +0330

    complete sidebar and create files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import Background from './Component/background/Background'
+import Sidebar from './Component/Sidebar/Sidebar'
+import './Fonts/BebasNeue-Regular.ttf'
+import { Container , Row , Col } from 'react-bootstrap'
 export default function App() {
   return (
     <div>
       <Background />
+      <Container>
+        <Row>
+          
+          <Col lg={2}>
+
+          <Sidebar />
+          </Col>
+
+        </Row>
+
+      </Container>
     </div>
   )
 }
--- src/Component/Sidebar/Sidebar.js
+++ src/Component/Sidebar/Sidebar.js
@@ -1,13 +1,48 @@
 import React from 'react'
+import './Sidebar.css'
+import Person3OutlinedIcon from '@mui/icons-material/Person3Outlined';
+import BusinessCenterOutlinedIcon from '@mui/icons-material/BusinessCenterOutlined';
+import SchoolOutlinedIcon from '@mui/icons-material/SchoolOutlined';
+import CodeOutlinedIcon from '@mui/icons-material/CodeOutlined';
+import CasesOutlinedIcon from '@mui/icons-material/CasesOutlined';
 
+import { Link } from 'react-router-dom';
 export default function Sidebar() {
   return (
     <div className='sidebar'>
-        <div className='menuSidebar'>
-            <div>
-                
+      <Link>
+      
+            <div className='menuTitle'>
+                <Person3OutlinedIcon />
+                <span>ABOUT</span>
             </div>
-        </div>
+      </Link>
+      <Link>
+      
+            <div className='menuTitle'>
+                <BusinessCenterOutlinedIcon />
+                <span>EXPERIENCE</span>
+            </div>
+      </Link>
+      <Link>
+      <div className='menuTitle'>
+                <SchoolOutlinedIcon />
+                <span>EDUCATION</span>
+            </div>
+      </Link>
+            <Link>
+            <div className='menuTitle'>
+                <CodeOutlinedIcon />
+                <span>KNOWLEDGE</span>
+            </div>
+            </Link>
+            <Link>
+            
+            <div className='lastMenuTitle'>
+                <CasesOutlinedIcon />
+                <span>PORTFOLIO</span>
+            </div>
+            </Link>
     </div>
   )
 }
