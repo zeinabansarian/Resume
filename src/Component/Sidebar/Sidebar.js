fea3683bec8c51b49961c4a7445feed230b7a8d8
zeinabansarian <zeinabansarian6280@gmail.com>
2023-06-11 21:00:42 +0330

    complete contact component

--- src/Component/Sidebar/Sidebar.js
+++ src/Component/Sidebar/Sidebar.js
@@ -1,40 +1,45 @@
-import React from 'react'
-import './Sidebar.css'
-import ImportantDevicesIcon from '@mui/icons-material/ImportantDevices';
-import Person3OutlinedIcon from '@mui/icons-material/Person3Outlined';
-import EventNoteIcon from '@mui/icons-material/EventNote';
-import CasesOutlinedIcon from '@mui/icons-material/CasesOutlined';
+import React from "react";
+import "./Sidebar.css";
+import ImportantDevicesIcon from "@mui/icons-material/ImportantDevices";
+import Person3OutlinedIcon from "@mui/icons-material/Person3Outlined";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import CasesOutlinedIcon from "@mui/icons-material/CasesOutlined";
+import MessageIcon from "@mui/icons-material/Message";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 export default function Sidebar() {
   return (
-    <div className='sidebar'>
-      <Link to={'/'}>
-            <div className='menuTitle'>
-                <Person3OutlinedIcon />
-                <span>ABOUT</span>
-            </div>
+    <div className="sidebar">
+      <Link to={"/"}>
+        <div className="menuTitle">
+          <Person3OutlinedIcon className="sideIcon"/>
+          <span>ABOUT</span>
+        </div>
       </Link>
-      <Link to={'/Experience'}>
-      
-            <div className='menuTitle'>
-                <EventNoteIcon />
-                <span>EXPERIENCE</span>
-            </div>
+      <Link to={"/Experience"}>
+        <div className="menuTitle">
+          <EventNoteIcon className="sideIcon"/>
+          <span>EXPERIENCE</span>
+        </div>
       </Link>
-            <Link to={'/Skills'}>
-            <div className='menuTitle'>
-                <ImportantDevicesIcon />
-                <span>Skills</span>
-            </div>
-            </Link>
-            <Link to={'/Portfolio'}>
-            
-            <div className='lastMenuTitle'>
-                <CasesOutlinedIcon />
-                <span>PORTFOLIO</span>
-            </div>
-            </Link>
+      <Link to={"/Skills"}>
+        <div className="menuTitle">
+          <ImportantDevicesIcon className="sideIcon"/>
+          <span>Skills</span>
+        </div>
+      </Link>
+      <Link to={"/Portfolio"}>
+        <div className="menuTitle">
+          <CasesOutlinedIcon className="sideIcon"/>
+          <span>PORTFOLIO</span>
+        </div>
+      </Link>
+      <Link to={"/Contact"}>
+        <div className="lastMenuTitle">
+          <MessageIcon className="sideIcon"/>
+          <span>CONTACT</span>
+        </div>
+      </Link>
     </div>
-  )
+  );
 }
--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import "./Contact.css";
+import HomeIcon from '@mui/icons-material/Home';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import CallIcon from '@mui/icons-material/Call';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import LocalPostOfficeIcon from '@mui/icons-material/LocalPostOffice';
 
 export default function Contact() {
   return (
-    <div>
-      
+    <div className="Contact">
+      <div className="ContactTitle">
+        <h2>CONTACT</h2>
+        <h2>اطلاعات تماس</h2>
+      </div>
+      <div className="contactBox">
+        <div className="contactDetail">
+          <HomeIcon className="contactIcon" />
+          <span>آدرس : </span>
+          <p>تهران ....</p>
+        </div>
+        <div className="contactDetail">
+          <CallIcon className="contactIcon"/>
+          <span>شماره تلفن : </span>
+          <p>09195292765</p>
+        </div>
+
+        <div className="contactDetail">
+          <WhatsAppIcon className="contactIcon"/>
+          <span>Whatsapp : </span>
+          <p>09195292765</p>
+        </div>
+        <div className="contactDetail">
+          <TelegramIcon className="contactIcon"/>
+          <span>Telegram : </span>
+          <p>09195292765</p>
+        </div>
+        <div className="contactDetail widthHundred">
+          <LocalPostOfficeIcon className="contactIcon"/>
+          <span>Email : </span>
+          <p>zeinabansarian6280@gmail.com</p>
+        </div>
+        <div className="contactDetail">
+          <GitHubIcon className="contactIcon"/>
+          <span>Git : </span>
+          <p>github.com/zeinabansarian</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
