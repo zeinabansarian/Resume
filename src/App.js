ddf35a6feeb98ceff6cf9e613de73c64f3ddd377
zeinabansarian <zeinabansarian6280@gmail.com>
2023-06-16 13:39:33 +0330

    complete mobile responsive

--- src/App.js
+++ src/App.js
@@ -3,30 +3,29 @@ import Background from "./Component/background/Background";
 import Sidebar from "./Component/Sidebar/Sidebar";
 import "./Fonts/BebasNeue-Regular.ttf";
 import routes from "./routes";
-import About from "./Pages/About/About";
 import { Container } from "react-bootstrap";
 import {Row} from "react-bootstrap";
 import {Col} from "react-bootstrap";
 import Profile from "./Component/Profile/Profile";
 import { useRoutes } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import './App.css'
 
 export default function App() {
   let router = useRoutes(routes);
   return (
-    <div>
+    <div className="app">
       <Background />
       <Container>
         <Row>
-          <Col xl={1} md={1}>
+          <Col xl={1} md={2} className="resPad">
             <Sidebar />
           </Col>
-          <Col xl={5} md={5}>
+          <Col xl={5} md={10} className="resPad">
 
             <Profile />
           </Col>
-          <Col xl={6} md={6}>
+          <Col xl={6} md={12} className="resPad">
           {router}
           </Col>
         </Row>
